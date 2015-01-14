9b4671933b435b2856ced55761aeb74edb20ce61
Rakesh <rakesh@moneypot.com>
2015-01-14 12:04:40 -0600

    test

--- client/scripts/game-logic/engine.js
+++ client/scripts/game-logic/engine.js
@@ -15,7 +15,7 @@ define([
     function Engine() {
         var self = this;
 
-        console.log(AppConstants.Engine.HOST);
+        console.log(window.document.location.host, AppConstants.Engine.HOST);
 
         /**
          * Engine inherits from BackBone events:
