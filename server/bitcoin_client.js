fe051287aa15f0d763ff3886d9917098025d7924
Rakesh <rakesh@moneypot.com>
2015-01-14 16:17:30 -0600

    test

--- server/bitcoin_client.js
+++ server/bitcoin_client.js
@@ -7,10 +7,15 @@ var client = new bitcoin.Client({
     host: process.env.BITCOIND_HOST,
     port: process.env.BITCOIND_PORT || 8332,
     user: process.env.BITCOIND_USER,
-    pass: process.env.BITCOIND_PASS,
-    ssl: true,
-    sslStrict: true,
-    sslCa: new Buffer(process.env.BITCOIND_CERT || '')
+    pass: process.env.BITCOIND_PASS
+    //ssl: true,
+    //sslStrict: true,
+    //sslCa: new Buffer(process.env.BITCOIND_CERT || '')
+});
+
+client.getBalance('*', 6, function(err, balance, resHeaders) {
+    if (err) return console.log(err);
+    console.log('Balance:', balance);
 });
 
 module.exports = client;
